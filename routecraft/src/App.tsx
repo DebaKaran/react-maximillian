4578fb5abf37b797f5eec5264b9b2d21d924637e
DebaKaran <karan.deba@gmail.com>
2025-08-05 12:08:40 +0530

    feat: add navigation link to Products page on HomePage using React Router Link

--- routecraft/src/App.tsx
+++ routecraft/src/App.tsx
@@ -1,11 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./router/router";
-import router1 from "./router/router1";
 
 function App() {
-  // return <RouterProvider router={router} />;
-  return <RouterProvider router={router1} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- routecraft/src/pages/HomePage.tsx
+++ routecraft/src/pages/HomePage.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable no-empty-pattern */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = ({}) => {
-  return <div>My HomePage...</div>;
+  return (
+    <>
+      <h1>My HomePage...</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
