95929e2405e241e1b29d714f33a57ee0eb512b84
DebaKaran <karan.deba@gmail.com>
2025-08-05 16:26:29 +0530

    feat: add dynamic product detail route with URL parameter support

--- routecraft/src/pages/Products.tsx
+++ routecraft/src/pages/Products.tsx
@@ -4,7 +4,16 @@ import React from "react";
 
 interface ProductsProps {}
 const Products: React.FC<ProductsProps> = ({}) => {
-  return <h1>Thr products</h1>;
+  return (
+    <>
+      <h1>Thr products</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
 };
 
 export default Products;
