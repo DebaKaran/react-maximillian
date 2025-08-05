d6d40ec9867517205cf15d1cdd5446422138c958
DebaKaran <karan.deba@gmail.com>
2025-08-05 11:25:02 +0530

    added routing for product component

---
+++ routecraft/src/pages/Products.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-empty-pattern */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import React from "react";
+
+interface ProductsProps {}
+const Products: React.FC<ProductsProps> = ({}) => {
+  return <h1>Thr products</h1>;
+};
+
+export default Products;
